a50d52c6d1f8a9b8f7430be37d9d077c629f9e3f
BoyerDamien <dboyer@student.42.fr>
2021-06-06 18:11:54 +0200

    add map bidirectionnal iterator

--- includes/iterators/map_iterator.hpp
+++ includes/iterators/map_iterator.hpp
@@ -6,7 +6,7 @@
 /*   By: dboyer <dboyer@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/05 09:33:43 by dboyer            #+#    #+#             */
-/*   Updated: 2021/06/05 16:32:26 by dboyer           ###   ########.fr       */
+/*   Updated: 2021/06/06 18:06:46 by dboyer           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,11 +36,11 @@ class map_iterator
     typedef typename base::size_type size_type;
     typedef bidirectional_iterator_tag iterator_category;
 
-    map_iterator( node_type *current ) : base( current ), _left( false ), _right( false )
+    map_iterator( node_type *current ) : base( current )
     {
     }
 
-    map_iterator( const self &other ) : base( other ), _left( other._left ), _right( other._right )
+    map_iterator( const self &other ) : base( other )
     {
     }
 
@@ -48,8 +48,6 @@ class map_iterator
     {
         this->setCurrent( other._current );
         this->setNull( other._null );
-        _left = other._left;
-        _right = other._right;
         return *this;
     }
 
@@ -60,29 +58,38 @@ class map_iterator
     /**************************************************************************
      *				Operator overloading
      *************************************************************************/
-    node_type *_next( node_type *current )
+    node_type *_next( node_type *current, bool down = false, bool up = false, bool left = false, bool right = false )
     {
         node_type *parent = current->parent();
-        if ( !_left && !_right && current->left() && !current->left()->left() )
-            return current->left();
-        if ( _left && current->right() && !current->right()->right() )
-            return current->right();
-        if ( parent && ( ( !current->right() && !current->left() ) || _right || ( _left && !current->right() ) ) )
-        {
-            _left = parent->left() == current;
-            _right = parent->right() == current;
-            if ( _left )
-                return parent;
-            return _next( parent );
-        }
-        if ( _left && current->right() )
-        {
-            _left = false;
-            _right = false;
-            return _next( current->right() );
-        }
-        std::cout << parent << " left: " << _left << " -- right: " << _right << std::endl;
-        return _next( current->left() );
+        if ( up && ( left || ( right && !parent ) ) )
+            return current;
+        if ( down && !current->left() )
+            return current;
+        if ( down && current->left() )
+            return _next( current->left(), true, false, true, false );
+        if ( parent && ( ( up && right ) || ( !current->left() && !current->right() ) ) )
+            return _next( parent, false, true, parent->left() == current, parent->right() == current );
+        if ( current->right() )
+            return _next( current->right(), true, false, false, true );
+        return current;
+    }
+    node_type *_previous( node_type *current, bool down = false, bool up = false, bool left = false,
+                          bool right = false )
+    {
+        node_type *parent = current->parent();
+        if ( up && ( right || ( left && !parent ) ) )
+            return current;
+        if ( down && !current->right() )
+            return current;
+        if ( down && current->right() )
+            return _previous( current->right(), true, false, false, true );
+        if ( ( left || !current->left() ) && up && parent )
+            return _previous( parent, false, true, parent->left() == current, parent->right() == current );
+        if ( ( ( !current->left() && !current->right() ) || !current->left() ) && parent )
+            return _previous( parent, false, true, parent->left() == current, parent->right() == current );
+        if ( current->left() )
+            return _previous( current->left(), true, false, true, false );
+        return current;
     }
 
     self &operator++( void )
@@ -92,13 +99,34 @@ class map_iterator
         return *this;
     }
 
+    self operator++( int )
+    {
+        self tmp = *this;
+        if ( this->getNode() )
+            this->setCurrent( _next( this->getNode() ) );
+        return tmp;
+    }
+    self &operator--( void )
+    {
+        if ( this->getNode() )
+            this->setCurrent( _previous( this->getNode() ) );
+        return *this;
+    }
+
+    self operator--( int )
+    {
+        self tmp = *this;
+        if ( this->getNode() )
+            this->setCurrent( _previous( this->getNode() ) );
+        return tmp;
+    }
+
     reference operator*( void ) const
     {
         return this->getNode()->getPair();
     }
 
   private:
-    bool _left, _right;
     map_iterator( void )
     {
     }
--- srcs/main.cpp
+++ srcs/main.cpp
@@ -6,11 +6,12 @@
 /*   By: dboyer <dboyer@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/10 16:45:01 by dboyer            #+#    #+#             */
-/*   Updated: 2021/06/05 16:50:37 by dboyer           ###   ########.fr       */
+/*   Updated: 2021/06/06 18:11:31 by dboyer           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_containers.hpp"
+#include "iterators/map_iterator.hpp"
 #include "list/list.hpp"
 #include "map/bst_element.hpp"
 #include "map/map.hpp"
@@ -27,23 +28,61 @@ int main( void )
 {
     /*if ( unittest::test_list() && unittest::test_vector() && unittest::test_pair() )
         return 0;*/
-    ft::bst_element< int, int > test;
-    test.setPair( ft::pair< int, int >( 5, 10 ) );
-    ft::bst_element< int, int > test2;
-    test2.setPair( ft::pair< int, int >( 11, 10 ) );
+    ft::bst_element< int, int > test1;
+    test1.setPair( ft::pair< int, int >( 1, 10 ) );
 
     ft::bst_element< int, int > test3;
-    test3.setPair( ft::pair< int, int >( 1, 10 ) );
+    test3.setPair( ft::pair< int, int >( 3, 10 ) );
 
-    test.setChild( &test2 );
-    test.setChild( &test3 );
-    ft::map_iterator< int, int > it( &test3 );
+    ft::bst_element< int, int > test2;
+    test2.setPair( ft::pair< int, int >( 2, 10 ) );
 
-    std::cout << *it << std::endl;
-    ++it;
-    std::cout << *it << std::endl;
-    ++it;
-    std::cout << *it << std::endl;
+    ft::bst_element< int, int > test6;
+    test6.setPair( ft::pair< int, int >( 6, 10 ) );
 
-    return -1;
+    ft::bst_element< int, int > test4;
+    test4.setPair( ft::pair< int, int >( 4, 10 ) );
+
+    ft::bst_element< int, int > test7;
+    test7.setPair( ft::pair< int, int >( 7, 10 ) );
+
+    ft::bst_element< int, int > test8;
+    test8.setPair( ft::pair< int, int >( 8, 10 ) );
+
+    ft::bst_element< int, int > test9;
+    test9.setPair( ft::pair< int, int >( 9, 10 ) );
+
+    ft::bst_element< int, int > test10;
+    test10.setPair( ft::pair< int, int >( 10, 10 ) );
+
+    ft::bst_element< int, int > test14;
+    test14.setPair( ft::pair< int, int >( 14, 10 ) );
+
+    ft::bst_element< int, int > test13;
+    test13.setPair( ft::pair< int, int >( 13, 10 ) );
+
+    test3.setChild( &test6 );
+    test3.setChild( &test1 );
+    test1.setChild( &test2 );
+    test6.setChild( &test4 );
+    test6.setChild( &test7 );
+    test8.setChild( &test3 );
+    test8.setChild( &test9 );
+    test9.setChild( &test10 );
+    test10.setChild( &test14 );
+    test14.setChild( &test13 );
+
+    ft::map_iterator< int, int > it( &test1 );
+
+    for ( int i = 0; i < 11; i++ )
+    {
+        std::cout << *it << std::endl;
+        it++;
+    }
+    for ( int i = 0; i < 11; i++ )
+    {
+        std::cout << *it << std::endl;
+        it--;
+    }
+    return 0;
 }
