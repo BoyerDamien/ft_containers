e695cebc7216a2af408c7903c59582d8d1f2a0a8
BoyerDamien <dboyer@student.42.fr>
2021-09-10 16:06:08 +0200

    42 vscode version

--- srcs/assert.cpp
+++ srcs/assert.cpp
@@ -6,14 +6,13 @@
 /*   By: dboyer <dboyer@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/24 14:17:50 by dboyer            #+#    #+#             */
-/*   Updated: 2021/07/23 11:21:20 by dboyer           ###   ########.fr       */
+/*   Updated: 2021/09/10 15:58:13 by dboyer           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_containers.hpp"
 
-void unittest::assert( bool expr, const std::string &message ) throw( std::exception )
-{
-    if ( !expr )
-        throw( unittest::FailedException( message ) );
+void unittest::assert(bool expr,
+                      const std::string &message) throw(std::exception) {
+  if (!expr) throw(unittest::FailedException(message));
 }
